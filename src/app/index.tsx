9f3c8d641de3ed432ddfb12b47a859134aba4923
Sathvik-Ganta100 <sathvikganta10@gmail.com>
2026-09-26 11:23:54 +0530

    Style sheet created

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+
+import { styles } from "../style/auth.style";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text style={styles.title}>Home</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
